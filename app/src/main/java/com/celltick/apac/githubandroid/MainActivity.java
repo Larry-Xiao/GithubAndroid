d3d116e8de2dbfa1171b8448b415fd42ae89d9df
larry10086xiao <larryx@aliyun.com>
2023-08-12 23:54:44 +0800

    added a button and changed the layout in activity_main.xml
    ->changed from original layout to Linearlayout.
    ->added a button.
    ->added finding btn object in MainActivity.java

--- app/src/main/java/com/celltick/apac/githubandroid/MainActivity.java
+++ app/src/main/java/com/celltick/apac/githubandroid/MainActivity.java
@@ -3,14 +3,18 @@ package com.celltick.apac.githubandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.button);
 
     }
 }
