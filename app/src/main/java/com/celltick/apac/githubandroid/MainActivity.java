4e65a8f60454e1197fcdd3fba1331e9212454c6a
xiaolei10086 <xiaolei10086@aliyun.com>
2023-08-14 12:23:47 +0800

    master branch hold by xiaolei has added a log in MainActivity.java

--- app/src/main/java/com/celltick/apac/githubandroid/MainActivity.java
+++ app/src/main/java/com/celltick/apac/githubandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.celltick.apac.githubandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.button);
+
+        Log.d("TAG","This log is from master org:xiaolei10086");
 
     }
 }
